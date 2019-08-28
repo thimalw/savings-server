78d8ab958d05d8bcc536f05a3e34998a42a2489a
Thimal Wickremage <thimalmadushanka@gmail.com>
2019-08-28 16:25:49 +1000

    clear database before each test run

--- tests/server.js
+++ tests/server.js
@@ -1,6 +1,17 @@
 const { expect } = require('chai');
+const mongoose = require('mongoose');
 const config = require('config');
+
+process.env.NODE_ENV = 'test';
 const server = require('../src/server');
+
+before((done) => {
+  mongoose.connection.once('open', () => {
+    mongoose.connection.db.dropDatabase(function() {
+      done();
+    });
+  });
+});
 
 describe('Server', () => {
   it('Should be running on the current port', async () => {
